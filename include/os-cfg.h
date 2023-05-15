1b019fbc6325bc20e5e007f6c471dbccf6e65042
TriCaoChanh <tri.caocs30503@hcmut.edu.vn>
2023-05-15 09:54:14 +0700

    change the queue logic

--- include/os-cfg.h
+++ include/os-cfg.h
@@ -5,7 +5,7 @@
 #define MAX_PRIO 140
 
 #define MM_PAGING 1
-// #define MM_FIXED_MEMSZ
+#define MM_FIXED_MEMSZ
 //#define VMDBG 1
 //#define MMDBG 1
 #define IODUMP 1
--- src/queue.c
+++ src/queue.c
@@ -12,31 +12,39 @@ int empty(struct queue_t *q)
 void enqueue(struct queue_t *q, struct pcb_t *proc)
 {
         /* TODO: put a new process to queue [q] */
-        // nếu queue đã đầy
         if (q->size == MAX_QUEUE_SIZE)
                 return;
-        // nếu queue còn trống
+
         if (q->size == 0)
         {
                 q->proc[0] = proc;
         }
         // nếu queue có phần tử và new proc priority nhỏ nhất
-        else if (proc->priority < q->proc[q->size - 1]->priority)
-        {
-                q->proc[q->size] = proc;
-        }
+        // else if (proc->priority < q->proc[q->size - 1]->priority)
+        // {
+        //         q->proc[q->size] = proc;
+        // }
         // nếu queue có phần tử và new proc priority nằm giữa
         else
         {
-                int i = 0;
-                while (proc->priority < q->proc[i]->priority)
+                // int i = 0;
+                // while (proc->priority < q->proc[i]->priority)
+                // {
+                //         i++;
+                // }
+                // for (int j = q->size; j > i; j--)
+                // {
+                //         q->proc[j] = q->proc[j - 1];
+                // }
+                // q->proc[i] = proc;
+                int i;
+                for (i = q->size; i > 0 && proc->priority >= q->proc[i - 1]->priority; i--)
                 {
-                        i++;
-                }
-                for (int j = q->size; j > i; j--)
-                {
-                        q->proc[j] = q->proc[j - 1];
-                }
+                        // In one queue, if 2 processes have the same priority number,
+                        // prioritize the ready one than the recently run one
+                        q->proc[i] = q->proc[i - 1];
+                } 
+
                 q->proc[i] = proc;
         }
         q->size++;
@@ -50,10 +58,8 @@ struct pcb_t *dequeue(struct queue_t *q)
         if (empty(q))
                 return NULL;
 
-        // lấy ở cuối queue
         struct pcb_t *proc = q->proc[q->size - 1];
 
-        // bỏ cái phần tử ra
         q->proc[q->size - 1] = NULL;
         q->size--;
         q->remaining_slots--;
